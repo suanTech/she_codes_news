d6241ce8fa30f2cc598aa478f157d9cb2eff551d
Suan Choi <siasiarhdwn@gmail.com>
2023-12-02 10:43:34 +0800

    Feat: add users app

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class NewsStory(models.Model):
+    # It will grab all the data sorted by order 
+    # class Meta:
+    #   order_by = 'pub_date'
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE
+        )
     pub_date = models.DateTimeField()
     content = models.TextField()
